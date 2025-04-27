86e073f8f3615507c9917a3757c6d9b91897d493
m g <mg@MBPsomtillhorm.lan>
2025-04-27 22:20:01 +0200

    created components for a recipe card and list. Added a constant folder for the urls.

---
+++ src/app/components/RecipeList.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { useState, useEffect } from "react";
+
+import RecipeCard from "./RecipeCard";
+import { BASE_IMG_URL, testData } from "@/constant/urls";
+
+
+interface Recipe {
+    id: number;
+    name: string;
+    type: string,
+    image: {
+        url: string;
+        name: string;
+    };
+    profiles: {
+        name: string;
+    }[];
+}
+
+
+const RecipeList = () => {
+    const [ recipes, setRecipes ] = useState<Recipe[]>([]);
+    const [ error, setError ] = useState<string | null>(null);
+    
+    useEffect(() => {
+        const fetchRecipes = async () => {
+            try {
+                const res = await fetch(testData);
+
+            if (!res.ok) {
+                throw new Error(`${res.status}`)
+            }
+
+            const data = await res.json();
+            setRecipes(data);
+        
+            // err must be type any
+            } catch (err: any) {
+                setError(err.message || "this is an error")
+            }
+        };
+
+        fetchRecipes();
+    }, []);
+
+    return (
+        <div>
+            {error && <p>Error: {error}</p>}
+            {recipes.map(recipe => (
+                <RecipeCard
+                    key={recipe.id}
+                    src={BASE_IMG_URL + recipe.image.url}
+                    alt={recipe.image.name}
+                    dishTitle={recipe.name}
+                    chef={recipe.profiles?.[0]?.name || null} 
+                    sponsored={false}
+                    type={recipe.type}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default RecipeList;
---
+++ src/constant/urls.ts
@@ -0,0 +1,3 @@
+// Not sure on how to use the img proxy, but I did choose a size that correlates to next.js's <Image> tag's width and height.
+export const BASE_IMG_URL = "https://img.koket.se/standard-medium/";
+export const testData = "https://gist.githubusercontent.com/andwin/4f52bc4bc951e5a36441ceee78a10989/raw/23452bd51782d4f32dffe503c44a9c3f3be3c2ca/testdata.json";
